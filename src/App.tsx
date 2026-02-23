244e0afed284183327523aab30e9a5fe4172f6c0
salempub1-dotcom <salempub1@gmail.com>
2026-02-23 02:33:09 +0100

    Delete src directory

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-// Updated notification message template to use customerWilayaLabel instead of customerWilaya.
-
-// Original code(segment around line 356)
-const notificationMessage = `${customerWilaya} has completed the action.`;
-
-// Updated code
-const notificationMessage = `${customerWilayaLabel} has completed the action.`;
